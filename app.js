b70e6e66b6d559ac1846221311621cc5f6ba0cb3
nahuelflores <arcangel6191@gmail.com>
2023-03-09 13:08:35 -0300

    Prueba de configuraciones para imagenes en S3 bucket

--- app.js
+++ app.js
@@ -32,23 +32,6 @@ mongoose.set('strictQuery', false);
 
 var app = express();
 
-// const nonce = crypto.randomBytes(16).toString('hex');
-app.use(helmet(
-  {
-    contentSecurityPolicy: {
-      directives: {
-        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
-        "img-src": ["'self'", "ebaproyect.s3.sa-east-1.amazonaws.com"],
-      },
-    },
-  }
-));
-
-// cf24ef80af08a52500a7a4eb189957883a8453b45e166d8faf808e0451c5510c
-app.use(cors({
-  origin: ['https://eba-proyect.onrender.com/', 'https://ebaproyect.s3.sa-east-1.amazonaws.com/']
-}));
-
 
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 app.use(express.static(__dirname + '/public'));
@@ -74,6 +57,33 @@ app.use(lingua(app, {
     //secure: true              // for serving over https
 }
 }));
+
+// const nonce = crypto.randomBytes(16).toString('hex');
+app.use(helmet(
+  {
+    contentSecurityPolicy: {
+      directives: {
+        ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+        "img-src": ["'self'", "data:","ebaproyect.s3.sa-east-1.amazonaws.com"],
+        "script-src": ["'self'"],
+      },
+    },
+  }
+));
+
+var whitelist = ['https://eba-proyect.onrender.com/*', 'https://ebaproyect.s3.sa-east-1.amazonaws.com/ADMINISTRACION.jpg']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+// cf24ef80af08a52500a7a4eb189957883a8453b45e166d8faf808e0451c5510c
+app.use(cors(corsOptions));
 
 app.use('/', indexRouter);
 app.use('/obras', obrasRouter);
