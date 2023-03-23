faa6e04477d7209915ba50bb5aa5fdd5294d93d7
nahuelflores <arcangel6191@gmail.com>
2023-03-23 15:05:55 -0300

    Pasado de imagenes en la seccion 'Nosotros' a bucket de amazon para cargas mas rapidas. Agregado de efecto de escala sobre las imagenes de las obras en su seccion de información.

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(helmet(
   }
 ));
 
-var whitelist = ['https://eba-proyect.onrender.com/*', 'https://ebaproyect.s3.sa-east-1.amazonaws.com/ADMINISTRACION.jpg']
+var whitelist = ['https://eba-proyect.onrender.com/*','https://ebaproyect.s3.sa-east-1.amazonaws.com/*']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1 || !origin) {
@@ -85,24 +85,6 @@ var corsOptions = {
 // cf24ef80af08a52500a7a4eb189957883a8453b45e166d8faf808e0451c5510c
 app.use(cors(corsOptions));
 
-// app.use(function (req, res, next) {
-
-//   // Website you wish to allow to connect
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
 
 app.use('/', indexRouter);
 app.use('/obras', obrasRouter);
